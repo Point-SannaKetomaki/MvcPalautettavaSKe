65f041810d90a4b2b5b746a3eee65fb7f8cdb750
Sanna Ketomaki <sanna.ketomaki@stud.pointcollege.fi>
2019-06-15 21:44:23 +0300

    Päivitetty EF Azuren tietokannasta

--- MvcPalautettavaSKe/MvcPalautettavaSKe/Models/ProjektitModel.Designer.cs
+++ MvcPalautettavaSKe/MvcPalautettavaSKe/Models/ProjektitModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\OWNER\Source\Repos\MvcPalautettavaSKe\MvcPalautettavaSKe\MvcPalautettavaSKe\Models\ProjektitModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\OWNER\source\repos\MvcPalautettavaSKe-Paranneltu\MvcPalautettavaSKe\MvcPalautettavaSKe\Models\ProjektitModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
